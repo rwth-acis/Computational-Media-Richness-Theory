7be96328af647ef5907a29da60332ed9bc41e8cb
DutchL <alex.devteam@gmail.com>
2016-02-01 18:20:57 +0100

    Monte Carlo - randomize communication frequencies

--- src/Main/MonteCarlo.java
+++ src/Main/MonteCarlo.java
@@ -1,10 +1,9 @@
 package Main;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
-
-import org.jgap.impl.IntegerGene;
-
+import java.util.Random;
 import DataLoader.DataMediator;
 import DataLoader.Results;
 import Media.AMedia;
@@ -21,6 +20,8 @@ import softwareSim.SoftweareSimBuilder;
 import softwareSim.Team;
 
 public class MonteCarlo {
+	private static List<List<Integer>> communicationFrequency;
+
 	public static void main(String[] args) {
 		File scenarioFile = new File(args[0]); // the scenario dir
 		String batchFile = "C:/Users/Alex/workspace/SoftwareSim/batch_external/batch_params.xml";
@@ -39,15 +40,11 @@ public class MonteCarlo {
 		for (String teamPath : Main.scenario.teamPaths) {
 			for (String projectPath : Main.scenario.projectPaths) {
 				for (String mediasPath : Main.scenario.mediaPaths) {
-					for (int runs = 0; runs < Main.scenario.maxAllowedEvolutions; runs++) {
-						List<MediaType> mt = Main.scenario
-								.getMedias(mediasPath);
+					List<MediaType> mt = Main.scenario.getMedias(mediasPath);
 
-						// (int)
-						// (Math.random()*30);
-						int i = 10;
-						int j = 10;
-						int k = 10;
+					// MonteCarlo.setFrequency(mt.size());
+					for (int runs = 0; runs < Main.scenario.maxAllowedEvolutions; runs++) {
+
 						// initialize the run
 						runner.runInitialize(batchFile);
 
@@ -60,15 +57,17 @@ public class MonteCarlo {
 
 						AMedia[] Medias = new AMedia[mt.size()];
 						for (int z = 0; z < mt.size(); z++) {
+							// int i = MonteCarlo.getFrequency(z);
+							int i = (int) (Math.random() * 30);
 							switch (mt.get(z)) {
 							case EMAIL:
 								Medias[z] = new Email(i);
 								break;
 							case PHONE:
-								Medias[z] = new Phone(j);
+								Medias[z] = new Phone(i);
 								break;
 							case FACETOFACE:
-								Medias[z] = new FaceToFace(k);
+								Medias[z] = new FaceToFace(i);
 								break;
 							}
 						}
@@ -101,7 +100,7 @@ public class MonteCarlo {
 										// scheduled
 										// at run end
 						runner.cleanUpRun();
-						System.out.println(">>> sim# " + i);
+						System.out.println(">>> sim# " + runs);
 					}
 					runner.cleanUpBatch(); // run after all runs complete
 				}
@@ -109,4 +108,56 @@ public class MonteCarlo {
 		}
 		System.out.println(">>> end");
 	}
+
+	/**
+	 * Initialize list with frequencies.
+	 * 
+	 * @param mediasCount
+	 */
+	private static void setFrequency(int mediasCount, int runs) {
+		MonteCarlo.communicationFrequency = new ArrayList<List<Integer>>();
+
+		int size = 30;
+		int z = runs / mediasCount;
+		for (int j = 0; j < mediasCount; j++) {
+			ArrayList<Integer> list = new ArrayList<Integer>(z);
+
+			if (z >= size) {
+				int t = z / size;
+				for (int i = 0; i < t; i++) {
+					for (int k = 0; k < size; k++) {
+						list.add(k);
+					}
+				}
+				for (int k = 0; k < z % size; k++) {
+					list.add((int) (Math.random() * size));
+				}
+
+			} else if (z < size) {
+				for (int i = 0; i < z; i++) {
+					list.add(i);
+				}
+			}
+
+			communicationFrequency.add(list);
+		}
+	}
+
+	/**
+	 * Get frequency from predefined list. No repeat.
+	 * 
+	 * @param fromList
+	 * @return
+	 */
+	private static int getFrequency(int fromList) {
+		List<Integer> list = MonteCarlo.communicationFrequency.get(fromList);
+		int index = -1;
+		Random rand = new Random();
+		if (list.size() > 0) {
+			index = rand.nextInt(list.size());
+			list.remove(index);
+		}
+		return index;
+	}
+
 }
